faae800e39c7bbf58ae25151279036cccb5bda6f
XBLToothPik <treyad@hotmail.com>
2016-09-11 20:53:54 -0400

    Added "rev" intrinstic

--- LoopConvert.cpp
+++ LoopConvert.cpp
@@ -2434,6 +2434,51 @@ public:
 				return false;
 			}
 			break;
+			case JoaatCasedConst("rev"):
+			{
+				if (argCount == 1)
+				{
+					if (argArray[0]->getType()->isIntegerType())
+					{
+						llvm::APSInt result;
+						if (argArray[0]->EvaluateAsInt(result, *context))
+						{
+							int64_t value = result.getSExtValue();
+							if (value >= 2)
+							{
+								LocalVariables.addLevel();
+								int startDeclIndex = LocalVariables.getCurrentSize();
+
+								//Add the vars
+								for (int i = 0; i < value; i++)
+									LocalVariables.addDecl("__rev-container-var-decl--" + to_string(i), 1);
+
+								//FromStack into it
+								out << iPush(value) << endl;
+								out << pFrame(startDeclIndex) << endl;
+								out << "FromStack" << endl;
+
+								//Put them back on stack in reverse
+								for (int i = startDeclIndex + value - 1; i >= startDeclIndex;i--)
+									out << frameGet(i) << endl;
+
+								LocalVariables.removeLevel();
+							}
+							else
+								Warn("Reverse called with " + to_string(value) + " exchange num.  Expected >= 2", rewriter, callee->getSourceRange());
+						}
+						else
+						{
+							Throw("rev must have signature \"extern __intrinsic void rev(const int numItems);\"", rewriter, callee->getSourceRange());
+							return false;
+						}
+					}
+					return true;
+				}
+				Throw("rev must have signature \"extern __intrinsic void rev(const int numItems);\"", rewriter, callee->getSourceRange());
+				return false;
+			}
+			break;
 			default:
 			_IntrinsicNotFound:
 				Throw("No intrinsic function found named " + funcName, rewriter, callee->getLocation());
