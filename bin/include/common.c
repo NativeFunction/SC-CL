fd71d421b0b6ac586819882cb26b2d3a2badc35d
Rock4259 <rock4259@hotmail.com>
2016-10-22 15:27:32 -0400

    added gtav pc hashes fixed issue #52

--- bin/include/common.c
+++ bin/include/common.c
@@ -6,20 +6,6 @@
 short int: SwapEndian16(x), unsigned short int: SwapEndian16(x),\
 default: SwapEndian32(x))
 
-#define typename(x) _Generic((x),\
-                                                                                  \
-        _Bool: "_Bool",                  unsigned char: "unsigned char",          \
-         char: "char",                     signed char: "signed char",            \
-    short int: "short int",         unsigned short int: "unsigned short int",     \
-          int: "int",                     unsigned int: "unsigned int",           \
-     long int: "long int",           unsigned long int: "unsigned long int",      \
-long long int: "long long int", unsigned long long int: "unsigned long long int", \
-        float: "float",                         double: "double",                 \
-  long double: "long double",                   char *: "pointer to char",        \
-       void *: "pointer to void",                int *: "pointer to int",         \
-      default: "other")
-
-
 int SwapEndian32(int value)
 {
 	return ((((value) & 0xff000000) >> 24) | (((value) & 0x00ff0000) >>  8) | (((value) & 0x0000ff00) <<  8) | (((value) & 0x000000ff) << 24));
