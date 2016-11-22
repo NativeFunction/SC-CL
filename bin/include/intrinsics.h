3abdbea9d489a85256fb5068f0d39483eafa2862
Rock4259 <rock4259@hotmail.com>
2016-11-22 12:54:53 -0500

    added vector2 math intrinsics

--- bin/include/intrinsics.h
+++ bin/include/intrinsics.h
@@ -37,12 +37,19 @@ extern __intrinsic const uint __varIndex(const char* varName);///Gets a var's in
 #pragma region Math/Conversions //{
 extern __intrinsic float reinterpretIntToFloat(int intValue);
 extern __intrinsic int reinterpretFloatToInt(float floatValue);
+//TODO: vector2 math
 extern __intrinsic vector3 toVector3(float value);
 extern __intrinsic vector3 vector3Add(vector3 left, vector3 right);
 extern __intrinsic vector3 vector3Sub(vector3 left, vector3 right);
 extern __intrinsic vector3 vector3Mult(vector3 left, vector3 right);
 extern __intrinsic vector3 vector3Div(vector3 left, vector3 right);
 extern __intrinsic vector3 vector3Neg(vector3 vector);
+extern __intrinsic vector2 toVector2(float value);
+extern __intrinsic vector2 vector2Add(vector2 left, vector2 right);
+extern __intrinsic vector2 vector2Sub(vector2 left, vector2 right);
+extern __intrinsic vector2 vector2Mult(vector2 left, vector2 right);
+extern __intrinsic vector2 vector2Div(vector2 left, vector2 right);
+extern __intrinsic vector2 vector2Neg(vector2 vector);
 extern __intrinsic float fMod(float divend, float divisor);
 extern __intrinsic bool bit_test(int value, const byte bitIndex);
 extern __intrinsic void bit_set(int* address, const byte bitIndex);
