043e9854818cb5f7ab235523c2d1ff025d4a631b
Rock4259 <rock4259@hotmail.com>
2017-04-21 16:56:38 -0400

    Updated varargs.h Updated CMakeLists.txt

--- bin/include/varargs.h
+++ bin/include/varargs.h
@@ -12,7 +12,6 @@
 #ifndef _VARARGS_H_
 #define _VARARGS_H_
 
-
 /*
 * I was told that Win NT likes this.
 */
@@ -25,42 +24,49 @@
 typedef int* va_list;
 #endif
 
-/*
-* Amount of space required in an argument list (ie. the stack) for an
-* argument of type t.
-*/
+/*************************************************************************
+	Amount of space required in an argument list (ie. the stack) for an
+	argument of type t.
+*************************************************************************/
 #define __va_argsiz(t)	\
 	(((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
 
 
-/*
-* Start variable argument list processing by setting AP to point to the
-* argument after pN.
-*/
+/*************************************************************************
+	Start variable argument list processing by setting AP to point to the
+	argument after pN.
+*************************************************************************/
 #define va_start(ap, pN)	\
 	((ap) = (va_list)*(va_list)((char*)(&pN) + __va_argsiz(pN)))
 
-/*
-* Increment ap to the next argument in the list while returing a
-* pointer to what ap pointed to first, which is of type t.
-*
-* We cast to void* and then to t* because this avoids a warning about
-* increasing the alignment requirement.
-*/
-
+/*************************************************************************
+	Increment ap to the next argument in the list while returing a
+	pointer to what ap pointed to first, which is of type t.
+*************************************************************************/
 #define va_arg(ap, t)					\
 	 (((ap) = (va_list)((char*)(ap) + __va_argsiz(t))),		\
-	  *((t*) (void*) ((char*)(ap) - __va_argsiz(t))))
+	  *((t*)((char*)(ap) - __va_argsiz(t))))
 
-/*
-* Gets the variable argument param count from the last argument pN
-*/
+/*************************************************************************
+	Initializes dest as a copy of src (in its current state)
+*************************************************************************/
+#define va_copy(dest, src) ((dest) = (src))
+
+/*************************************************************************
+	End processing of variable argument list. In this case we do nothing.
+*************************************************************************/
+#define va_end(ap)	((void)0)
+
+
+/*************************************************************************
+	Gets the variable argument param count from the last argument pN
+*************************************************************************/
 #define va_count(pN)	\
 	(*(int*)((char*)(&pN) + __va_argsiz(pN) + sizeof(va_list)))
 
-/*
-* Gets the variable argument stack count from the last argument pN
-*/
+/*************************************************************************
+	Gets the variable argument stack count from the last argument pN
+*************************************************************************/
 #define va_scount(pN)	\
 	(*(int*)((char*)(&pN) + __va_argsiz(pN) + sizeof(va_list) + sizeof(int)))
 
