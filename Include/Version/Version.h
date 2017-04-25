d0acffa5881235f6306428a793b2a46b27c74dcd
Rock4259 <rock4259@hotmail.com>
2017-04-24 20:37:36 -0400

    Added Version count + 1 to provide correct version on a commit

--- Include/Version/Version.h
+++ Include/Version/Version.h
@@ -2,10 +2,10 @@
 * Version.h
 *
 * This file is not to be included in the project
-* to prevent have to reload every build.
+* to prevent having to reload every build.
 *
 */
 #pragma once
 
 //major.minor.revision.patchlevel
-#define VERSION "0.7.0.5"
+#define VERSION "0.7.0.7"
